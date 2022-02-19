fc257065d23fcbb792d208ae78a781d40c12bda8
sarangsk01 <95225059+sarangsk01@users.noreply.github.com>
2022-02-19 18:37:39 +0530

    Update index.js

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,11 +3,28 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
-
+for(let i=1; i<11; i=i+1){
+    console.log(i);
+}
 // Using `console.log` log all the evan values from 1 to 10.
-
+for(let i=1; i<11; i=i+1){
+    if (i % 2 ==0){
+    console.log(i);
+}
+}
 // Using `console.log` log all the od values from 1 to 10.
-
+for(let i=1; i<11; i=i+1){
+    if (i % 2 !==0){
+    console.log(i);
+}
+}
 // Calculate the sum of all numbers from 1 to 10.
 
+
 // Log all the values from 1 to 10 using while loop
+while(i<11){
+    if(i %2 ==1){
+    console.log(i);
+}
+    i =i+1;
+}
